4a0da862fed4f97b519a09f345cb7e8bd00a3593
SotaLab-dev <nmst1228@outlook.jp>
2026-07-26 05:20:41 +0900

    feat：テキストボックスのUIを定義

---
+++ frontend/src/components/ui/AppTextField.tsx
@@ -0,0 +1,45 @@
+import TextField from "@mui/material/TextField";
+
+type AppTextFieldProps = {
+    // 必須Props
+    label: string;
+    value: string;
+    // 見た目に関するProps
+    placeholder?: string;
+    type?: string;
+    // 状態に関するProps
+    required?: boolean;
+    disabled?: boolean;
+    error?: boolean;
+    helperText?: string;
+    // イベント
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const AppTextField = ({
+    label,
+    value,
+    onChange,
+    placeholder,
+    type = "text",
+    required = false,
+    disabled = false,
+    error = false,
+    helperText,
+}: AppTextFieldProps) => {
+    return (
+        <TextField
+            label={label}
+            value={value}
+            onChange={onChange}
+            placeholder={placeholder}
+            type={type}
+            required={required}
+            disabled={disabled}
+            error={error}
+            helperText={helperText}
+        />
+    );
+};
+
+export default AppTextField;
