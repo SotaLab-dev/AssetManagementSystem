24b5c532ec56c163fa4f125b98fdf8e835e413c3
SotaLab-dev <nmst1228@outlook.jp>
2026-07-29 01:27:16 +0900

    refactor：AppTextFieldの再利用性を改善する

--- frontend/src/components/ui/AppTextField.tsx
+++ frontend/src/components/ui/AppTextField.tsx
@@ -1,50 +1,17 @@
 import TextField from "@mui/material/TextField";
-import type { TextFieldProps }  from "@mui/material/TextField";
+import type { TextFieldProps } from "@mui/material/TextField";
 
-type AppTextFieldProps = {
-    // 必須Props
+type AppTextFieldProps = Omit<
+    TextFieldProps,
+    "label"
+> & {
     label: string;
-    value: string;
-    // 見た目に関するProps
-    placeholder?: string;
-    type?: TextFieldProps["type"];
-    multiline?: TextFieldProps["multiline"];
-    rows?: TextFieldProps["rows"];
-    // 状態に関するProps
-    required?: TextFieldProps["required"];
-    disabled?: TextFieldProps["disabled"];
-    error?: boolean;
-    helperText?: string;
-    // イベント
-    onChange?: TextFieldProps["onChange"];
 };
 
-const AppTextField = ({
-    label,
-    value,
-    onChange,
-    placeholder,
-    type = "text",
-    required = false,
-    disabled = false,
-    error = false,
-    helperText,
-    multiline = false,
-    rows
-}: AppTextFieldProps) => {
+const AppTextField = (props: AppTextFieldProps) => {
     return (
         <TextField
-            label={label}
-            value={value}
-            onChange={onChange}
-            placeholder={placeholder}
-            type={type}
-            required={required}
-            disabled={disabled}
-            error={error}
-            helperText={helperText}
-            multiline={multiline}
-            rows={rows}
+            {...props}
         />
     );
 };
--- frontend/src/pages/Asset/components/AssetForm.tsx
+++ frontend/src/pages/Asset/components/AssetForm.tsx
@@ -7,9 +7,14 @@ import {
 import AppButton from "../../../components/ui/AppButton";
 import AppSelect from "../../../components/ui/AppSelect";
 import AppTextField from "../../../components/ui/AppTextField";
-import { assetCategoryOptions, assetStatusOptions, DEFAULT_ASSET_STATUS } from "../../../constants/Asset";
+import { assetCategoryOptions, assetStatusOptions, DEFAULT_ASSET_STATUS, MAX_ASSET_NAME_LENGTH, MAX_MANAGEMENT_NO_LENGTH, MAX_REMARKS_LENGTH } from "../../../constants/Asset";
+import { useState } from "react";
 
 const AssetForm = () => {
+    const [assetName, setAssetName] = useState<string>("");
+    const [managementNumber, setManagementNumber] = useState<string>("");
+    const [purchaseDate, setPurchaseDate] = useState<Date | null>(null);
+    const [remarks, setRemarks] = useState<string>("");
     return (
         <Card>
             <CardContent>
@@ -17,8 +22,16 @@ const AssetForm = () => {
 
                     <AppTextField
                         label="備品名"
-                        value=""
+                        value={assetName}
                         required
+                        slotProps={{
+                            htmlInput: {
+                                maxLength: MAX_ASSET_NAME_LENGTH,
+                            },
+                        }}
+                        onChange={(event) => {
+                            setAssetName(event.target.value);
+                        }}
                     />
 
                     <AppSelect
@@ -35,20 +48,39 @@ const AssetForm = () => {
 
                     <AppTextField
                         label="管理番号"
-                        value=""
+                        value={managementNumber}
+                        required
+                        slotProps={{
+                            htmlInput: {
+                                maxLength: MAX_MANAGEMENT_NO_LENGTH,
+                            },
+                        }}
+                        onChange={(event) => {
+                            setManagementNumber(event.target.value);
+                        }}
                     />
 
                     <AppTextField
                         label="購入日"
-                        value=""
-                        type="date"
+                        value={purchaseDate ? purchaseDate.toISOString().split('T')[0] : "" }
+                        required
+                        onChange={(event) => {
+                            setPurchaseDate(event.target.value ? new Date(event.target.value) : null);
+                        }}
                     />
 
                     <AppTextField
                         label="備考"
-                        value=""
-                        multiline
-                        rows={4}
+                        value={remarks}
+                        required
+                        slotProps={{
+                            htmlInput: {
+                                maxLength: MAX_REMARKS_LENGTH,
+                            },
+                        }}
+                        onChange={(event) => {
+                            setRemarks(event.target.value);
+                        }}
                     />
 
                     <Stack
