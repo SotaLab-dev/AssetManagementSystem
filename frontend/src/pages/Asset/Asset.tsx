333b140941c966ed30d6aa0c40cce6e1c7e3b88b
SotaLab-dev <nmst1228@outlook.jp>
2026-08-12 07:25:57 +0900

    feat：備品一覧画面でページング機能を実装

--- frontend/src/pages/Asset/Asset.tsx
+++ frontend/src/pages/Asset/Asset.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import type { AssetSearchCondition } from "../../types/Asset";
 
@@ -9,6 +10,9 @@ import AssetToolbar from "./AssetToolbar";
 import { type AssetLayoutContext } from "../../types/Asset";
 
 const Asset = () => {
+    const [page, setPage] = useState<number>(0);
+    const [rowsPerPage, setRowsPerPage] = useState<number>(10);
+
     const {
         assets,
         setAssets,
@@ -33,7 +37,7 @@ const Asset = () => {
 
     const handleSearch = () => {
         setAppliedSearchCondition(searchCondition);
-
+        setPage(0);
     };
 
     const handleReset = () => {
@@ -44,6 +48,7 @@ const Asset = () => {
         };
         setSearchCondition(emptyCondition);
         setAppliedSearchCondition(emptyCondition);
+        setPage(0);
     }
 
     const filteredAssets = assets.filter((asset) => {
@@ -69,6 +74,27 @@ const Asset = () => {
         )
     })
 
+    const paginatedAssets = filteredAssets.slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage,
+    );
+
+    const handleChangePage = (
+        _event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number,
+    ) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    }
+
+
     return (
         <Stack spacing={3}>
             <AssetToolbar />
@@ -79,8 +105,13 @@ const Asset = () => {
             />
 
             <AssetTable
-                assets={filteredAssets}
+                assets={paginatedAssets}
                 onDelete={handleDelete}
+                count={filteredAssets.length}
+                page={page}
+                onPageChange={handleChangePage}
+                rowsPerPage={rowsPerPage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
             />
         </Stack>
     );
--- frontend/src/pages/Asset/AssetTable.tsx
+++ frontend/src/pages/Asset/AssetTable.tsx
@@ -11,6 +11,7 @@ import {
     TableContainer,
     TableHead,
     TableRow,
+    TablePagination,
 } from "@mui/material";
 
 import { useNavigate } from "react-router-dom";
@@ -20,92 +21,121 @@ import type { AssetItem } from "../../types/Asset";
 
 type AssetTableProps = {
     assets: AssetItem[];
+    count: number;
+    page: number;
+    rowsPerPage: number;
+    onPageChange: (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        page: number
+    ) => void;
+    onRowsPerPageChange: (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => void;
     onDelete?: (id: string) => void;
 };
 
-const AssetTable = ({assets, onDelete}: AssetTableProps) => {
+const AssetTable = ({
+    assets,
+    count,
+    page,
+    rowsPerPage,
+    onPageChange,
+    onRowsPerPageChange,
+    onDelete
+}: AssetTableProps) => {
     const navigate = useNavigate();
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell padding="checkbox">
-                            <Checkbox />
-                        </TableCell>
-                        <TableCell>ID</TableCell>
-                        <TableCell>備品名</TableCell>
-                        <TableCell>カテゴリ</TableCell>
-                        <TableCell>状態</TableCell>
-                        <TableCell align="center">
-                            操作
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-
-                <TableBody>
-                    {assets.length === 0 ? (
+        <>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
                         <TableRow>
-                            <TableCell
-                                colSpan={6}
-                                align="center"
-                            >
-                                データがありません。
+                            <TableCell padding="checkbox">
+                                <Checkbox />
+                            </TableCell>
+                            <TableCell>ID</TableCell>
+                            <TableCell>備品名</TableCell>
+                            <TableCell>カテゴリ</TableCell>
+                            <TableCell>状態</TableCell>
+                            <TableCell align="center">
+                                操作
                             </TableCell>
                         </TableRow>
-                    ) : (
-                        assets.map((asset) => (
-                            <TableRow
-                                hover
-                                key={asset.id}
-                            >
-                                <TableCell padding="checkbox">
-                                    <Checkbox />
-                                </TableCell>
-                                <TableCell>{asset.id}</TableCell>
+                    </TableHead>
 
-                                <TableCell>
-                                    {asset.assetName}
-                                </TableCell>
-
-                                <TableCell>
-                                    {asset.category}
-                                </TableCell>
-
-                                <TableCell>
-                                    <StatusChip status={asset.status} />
-                                </TableCell>
-
+                    <TableBody>
+                        {assets.length === 0 ? (
+                            <TableRow>
                                 <TableCell
+                                    colSpan={6}
                                     align="center"
-                                    width={120}
                                 >
-                                    <IconButton
-                                        color="primary"
-                                        aria-label="編集"
-                                        onClick={() => {
-                                            navigate(`/asset/edit/${asset.id}`);
-                                        }}
-                                    >
-                                        <EditIcon />
-                                    </IconButton>
-
-                                    <IconButton
-                                        color="error"
-                                        aria-label="削除"
-                                        onClick={() => {
-                                            onDelete?.(asset.id);
-                                        }}
-                                    >
-                                        <DeleteIcon />
-                                    </IconButton>
+                                    データがありません。
                                 </TableCell>
                             </TableRow>
-                        ))
-                    )}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                        ) : (
+                            assets.map((asset) => (
+                                <TableRow
+                                    hover
+                                    key={asset.id}
+                                >
+                                    <TableCell padding="checkbox">
+                                        <Checkbox />
+                                    </TableCell>
+                                    <TableCell>{asset.id}</TableCell>
+
+                                    <TableCell>
+                                        {asset.assetName}
+                                    </TableCell>
+
+                                    <TableCell>
+                                        {asset.category}
+                                    </TableCell>
+
+                                    <TableCell>
+                                        <StatusChip status={asset.status} />
+                                    </TableCell>
+
+                                    <TableCell
+                                        align="center"
+                                        width={120}
+                                    >
+                                        <IconButton
+                                            color="primary"
+                                            aria-label="編集"
+                                            onClick={() => {
+                                                navigate(`/asset/edit/${asset.id}`);
+                                            }}
+                                        >
+                                            <EditIcon />
+                                        </IconButton>
+
+                                        <IconButton
+                                            color="error"
+                                            aria-label="削除"
+                                            onClick={() => {
+                                                onDelete?.(asset.id);
+                                            }}
+                                        >
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <TablePagination
+                component="div"
+                count={count}
+                page={page}
+                onPageChange={onPageChange}
+                rowsPerPage={rowsPerPage}
+                onRowsPerPageChange={onRowsPerPageChange}
+            />
+        </>
     );
 };
 
