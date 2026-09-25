5c56f751c13d5746936e5bbacbfacdcb26b34d43
SotaLab-dev <nmst1228@outlook.jp>
2026-09-25 15:39:18 +0900

    fix：Dialog共通化漏れを修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
+import { DialogProvider } from "./contexts/DialogProvider";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <AppRouter />
+            <DialogProvider>
+                <AppRouter />
+            </DialogProvider>
         </BrowserRouter>
     );
 };
